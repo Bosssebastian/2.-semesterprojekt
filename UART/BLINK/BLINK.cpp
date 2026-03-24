753683d53915cab46e3ccb5b41e269e0425bfd84
Jakob Friborg Møller <jakmo25@student.sdu.dk>
2026-03-24 12:19:15 +0100

    fixed serial plotter

--- UART/BLINK/BLINK.cpp
+++ UART/BLINK/BLINK.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
 int main()
 {
     stdio_init_all();
+    sleep_ms(2000);   // gives USB serial time to appear after reset
 
     const uint LED_PIN = 25;
 
@@ -13,14 +13,11 @@ int main()
 
     while (true) {
         printf("Hello, world!\n");
-        sleep_ms(1000);
 
-        gpio_put(LED_PIN, 1);  // LED ON
-        sleep_ms(500);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(200);
 
-        gpio_put(LED_PIN, 0);  // LED OFF
-        sleep_ms(500);
-
+        gpio_put(LED_PIN, 0);
+        sleep_ms(200);
     }
 }
-
