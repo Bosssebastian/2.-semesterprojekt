0b9dc398b4d46829958b77485f00492a14344252
Mathies Toftgård <mathiestoftgaard@live.dk>
2026-04-07 13:04:10 +0200

    Clean up of uart branch before merge

--- shared/uartClass.h
+++ shared/uartClass.h
@@ -33,12 +33,6 @@ public:
     std::string readPackage();
     bool hasPackage();
 
-    void clearRXQue();
-    //&result is reference parameter used as output parameter
-    bool requestPackage(uint8_t msgType, uint8_t &result, uint32_t ackTimeout_ms = 500, uint32_t resTimeout_ms = 500);
-
-    bool tryReadPackage(uint8_t &flag, uint8_t &msgType, uint8_t &data);
-
 private:
     uart_inst_t* uart = nullptr;
     int uart_id;
