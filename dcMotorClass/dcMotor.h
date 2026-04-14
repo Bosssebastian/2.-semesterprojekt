7aa9cf5e3f4173320e86fb020f9024729b3981d5
Your Name <you@example.com>
2026-04-14 13:58:19 +0200

    .h is done i think, yet to fix .cpp and main

---
+++ dcMotorClass/dcMotor.h
@@ -0,0 +1,17 @@
+#ifndef DCMOTOR_H
+#define DCMOTOR_H
+
+class DcMotor {
+public:
+    DcMotor(uint pwmPin, uint input1, uint input2);
+    void forwards();
+    void backwards();
+    void stop();
+
+private:
+    uint pwmPin;
+    uint input1;
+    uint input2;
+};
+
+#endif
