f52f28b243b12ccc6c53c8471c79bfd20358a001
Sebastian Yde Junker <sebastianydejunker@gmail.com>
2026-04-21 13:52:43 +0200

    swapped away from json

--- pc/src/vision/VisionController.cpp
+++ pc/src/vision/VisionController.cpp
@@ -1,6 +1,7 @@
 #include "VisionController.h"
 #include <iostream>
 
+/* just in case
 std::string getJsonValue(const std::string& json, const std::string& key) {
     std::string searchKey = "\"" + key + "\":";
     size_t startPos = json.find(searchKey);
@@ -14,6 +15,21 @@ std::string getJsonValue(const std::string& json, const std::string& key) {
     
     return json.substr(valStart, valEnd - valStart);
 }
+*/
+
+void getValue(std::string& value, std::string& object, std::string& color, int& size)
+/*input string, object type string, color string, size int*/
+{
+    object = value.substr(0,3);
+    color = value.substr(4,3);
+    size = stoi(value.substr(8,(value.length()-8)));
+    return;
+}
+
+
+
+
+
 
 
 VisionController::VisionController()
@@ -41,12 +57,18 @@ VisionController::VisionController()
     // turn off autofocus
     mCam->set(cv::CAP_PROP_AUTOFOCUS, 0);
 
+
+    //
+    //mCam->set(cv::CAP_PROP_SATURATION,)
+
     // give the camera time to start correctly
     for (int i = 0; i < 30; i++) {
         mCam->grab(); // Just grab the data, don't decode it yet to save time
     }
+
     mCam->retrieve(mFrame);
     cv::imwrite("fixed_capture.jpg", mFrame);
+  
 }
 
 VisionController::~VisionController()
@@ -63,7 +85,9 @@ void VisionController::scanForObject()
 
     // take an image and save it as mFrame
     mCam->grab();
-    mCam->retrieve(mFrame);
+    mCam->retrieve(mTempFrame);
+
+     cv::cvtColor(mTempFrame, mFrame, cv::COLOR_BGR2GRAY);
 
     // checks if an image was taken if not then returns
     if (mFrame.empty()) {
@@ -80,7 +104,6 @@ void VisionController::scanForObject()
         mCorners.release();
         return;
     }
-    std::cout << mData << "\n";
 
     mX = 0;
     mY = 0;
@@ -97,17 +120,22 @@ void VisionController::scanForObject()
             }
         }
 
-        if (pointCount > 0) {
-            mX /= static_cast<double>(pointCount);
-            mY /= static_cast<double>(pointCount);
+        if (pointCount == 4) {
+            mX /= static_cast<double>(4);
+            mY /= static_cast<double>(4);
             std::cout << "x: " << mX << "\ny: " << mY << "\n";
         }
     }
 
     if (!mData.empty()) {
+
+        getValue(mData, mObject, mColor, mSize);
+
+        /*
         mObject = getJsonValue(mData, "object");
         mSize = getJsonValue(mData, "size");
         mColor = getJsonValue(mData, "color");
+        */
         mStatus = true;
         std::cout << "object: " << mObject << "\nsize: " << mSize << "\ncolor: " << mColor << "\n";
     }
@@ -118,7 +146,7 @@ bool VisionController::objectReady()
     return mStatus;
 }
 
-void VisionController::getObjectPosition(double outputPos[2][1])
+void VisionController::getObjectPosition(std::vector<std::vector<double>> outputPos)
 {
     outputPos[0][0] = mX;
     outputPos[1][0] = mY;
--- pc/src/vision/VisionController.h
+++ pc/src/vision/VisionController.h
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 class VisionController
 {
@@ -9,18 +10,19 @@ public:
     void scanForObject();
 
     bool objectReady();
-    void getObjectPosition(double outputPos[2][1]);
+    void getObjectPosition(std::vector<std::vector<double>> outputPos);
     void getObjectInfo(std::string&, std::string&, std::string&);
 
 private:
     cv::VideoCapture* mCam;
     cv::QRCodeDetector mQR;
-    cv::Mat mFrame, mCorners;
+    cv::Mat mTempFrame, mFrame, mCorners;
 
     bool mStatus = false;    
     
     std::string mData;
-    std::string mObject, mSize, mColor;
+    std::string mObject, mColor;
+    int mSize;
     
     cv::Point2d mPos;
     double mX, mY;
