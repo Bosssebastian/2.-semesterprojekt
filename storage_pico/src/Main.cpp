f1e02c9f735d8a4afbd18f71f1be5763c8f31a3e
Your Name <you@example.com>
2026-04-23 08:25:41 +0200

    making files work together

--- storage_pico/src/Main.cpp
+++ storage_pico/src/Main.cpp
@@ -1,7 +1,12 @@
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "dcMotor.h"
+
 
 int main() {
     stdio_init_all();
+    DcMotor motor(0, 1, 11);
+    motor.forwards();
 
 
     while (true) {
