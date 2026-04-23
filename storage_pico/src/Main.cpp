7e7a6e22eb6cf59c5ff584d5cccde43bb52eba3b
Your Name <you@example.com>
2026-04-23 15:33:51 +0200

    testadded

--- storage_pico/src/Main.cpp
+++ storage_pico/src/Main.cpp
@@ -1,13 +1,18 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "dcMotor.h"
+#include "pathFinder.h"
+#include <iostream>
 
 
 int main() {
     stdio_init_all();
     DcMotor motor(0, 1, 11);
     motor.forwards();
-    pathFinder shortcut(0, 0, 8);
+    sleep_ms(5000);
+    motor.forwards();
+    sleep_ms(5000);
+    pathFinder shortcut(0, 3, 8);
     int target;
     //in shortcut first number is target position, second number is current position and last number
     //is max number og positions
