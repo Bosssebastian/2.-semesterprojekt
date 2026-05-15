69213e2179c54d21254fd04023de2c3fa1b154ad
Alexander <alkam25@student.sdu.dk>
2026-05-15 10:51:46 +0200

    test

--- DataBase/main.cpp
+++ DataBase/main.cpp
@@ -6,7 +6,7 @@ int main() {
     sql.createTables();
     sql.updateStorageSlot(1, 4, 1); // storage id, object id, occupied state
     sql.insertVisionObject("cube", 5, "red"); //object, size, color
-    sql.insertHistory(4,1, 12.33); // object id, slot, timestamp (double)
+    sql.insertHistory(4, 1, 12.33); // object id, slot, timestamp (double)
 
     
     return 0;
