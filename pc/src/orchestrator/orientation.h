691444607ba2f2784c5b9e8c9b718b2a395d5cef
spnrnav <anmil25@student.sdu.dk>
2026-05-18 14:09:20 +0200

    added clarifications

--- pc/src/orchestrator/orientation.h
+++ pc/src/orchestrator/orientation.h
@@ -6,7 +6,7 @@ public:
     Orientation(){};
     double norm(std::vector<double>);
     std::vector<std::vector<double>> getRotMat(std::vector<double>, double);
-    std::vector<double> rotMat2rv(std::vector<std::vector<double>>);
+    std::vector<double> rotMat2rv(std::vector<std::vector<double>>); // Outdated
     std::vector<double> rotMat2rv2(std::vector<std::vector<double>>);
     std::vector<std::vector<double>> compose(std::vector<std::vector<double>>, std::vector<std::vector<double>>);
     std::vector<double> getAngleAxisVector(double rotationZ = 0.39269908169872415480783042290994, bool deg = false);
