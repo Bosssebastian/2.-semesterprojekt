28cf034c5b23bfd03d6c667c48b2f618f4c528d5
Alexander <alkam25@student.sdu.dk>
2026-05-18 11:19:27 +0200

    small fixes

--- pc/src/orchestrator/database.h
+++ pc/src/orchestrator/database.h
@@ -17,7 +17,9 @@ public:
 
     bool execute(const std::string& sql);
     void createTables();
+
     int insertVisionObject(const std::string& object, std::string size, const std::string& color); 
+
     void updateStorageSlot(int slotId, int objectId, bool occupied);
     void insertHistory(int objectId, int slot, double timestamp);
 
